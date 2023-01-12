2aef0e14790b3b1e2c6ef54dc1abc7ce4c59445b
Dmytro-Ivanenko <dmitro0399@gmail.com>
2023-01-12 23:13:16 +0200

    base structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,24 @@
+import Profile from 'components/Profile/Profile';
+import Statistics from 'components/Statistics/Statistics';
+import FriendList from 'components/FriendList/FriendList';
+import TransactionHistory from 'components/TransactionHistory/TransactionHistory';
+import user from 'components/Profile/user.json';
+
 export const App = () => {
+  const { username, tag, location, avatar, stats } = user;
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Profile
+        username={username}
+        tag={tag}
+        location={location}
+        avatar={avatar}
+        stats={stats}
+      />
+      <Statistics />
+      <FriendList />
+      <TransactionHistory />
+    </>
   );
 };
