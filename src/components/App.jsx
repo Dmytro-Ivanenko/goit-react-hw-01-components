1caca42ade309f3a8bf6e0d7be324ee32079b7ef
Dmytro-Ivanenko <dmitro0399@gmail.com>
2023-01-14 21:38:44 +0200

    update structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import { Profile } from 'components/Profile/Profile';
 // import Statistics from 'components/Statistics/Statistics';
 // import FriendList from 'components/FriendList/FriendList';
 // import TransactionHistory from 'components/TransactionHistory/TransactionHistory';
-import user from 'components/Profile/user.json';
+import user from '../data/user.json';
 
 export const App = () => {
   const { username, tag, location, avatar, stats } = user;
