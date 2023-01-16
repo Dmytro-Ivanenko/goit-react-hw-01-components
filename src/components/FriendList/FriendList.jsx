e9ad2cd8dbc2793e6918051de779686608035b6d
Dmytro-Ivanenko <dmitro0399@gmail.com>
2023-01-16 20:04:04 +0200

    Update FriendList.jsx

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -23,7 +23,7 @@ FriendList.propTypes = {
       avatar: PropTypes.string.isRequired,
       name: PropTypes.string.isRequired,
       isOnline: PropTypes.bool.isRequired,
-      id: PropTypes.number,
+      id: PropTypes.number.isRequired,
     })
   ).isRequired,
 };
